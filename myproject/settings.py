c150cb378b214d4e4895c5237f165f56dfca2c33
faysalhassantorjo <faysalhassantorjo8@gmail.com>
2023-08-28 02:08:54 +0600

    media file 3

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Separate directory for development static files
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # django_heroku.settings(locals())
