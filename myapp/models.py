6a71c07c7c221a36580a6ddd55c5be1e29a0c6db
faysalhassantorjo <faysalhassantorjo8@gmail.com>
2023-08-28 11:19:03 +0600

    data base field

--- myapp/models.py
+++ myapp/models.py
@@ -22,7 +22,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='media',null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
@@ -41,7 +41,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description=models.CharField(default="",max_length=200)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to="media/",null=True, blank=True)
 
     price = models.IntegerField(default=0)
     instock=models.IntegerField(default=0)
